4ccfd8025f025806cf5689a50b3aa0e76369b58b
jholloway <jholloway@griddynamics.com>
2023-01-18 12:23:16 -0500

    create Form component

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+export default function Form(){
+    return (
+        <form className="form-section">
+            <div className="input-fields">
+                <input type="text" placeholder="Enter text here"></input>
+                <input type="text" placeholder="Enter text here"></input>
+            </div>
+            <input type="submit"></input>
+        </form>
+    )
+}
