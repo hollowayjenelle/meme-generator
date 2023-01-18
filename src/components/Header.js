5abb3cd9828dddfa0c40457576f0337d648f4320
jholloway <jholloway@griddynamics.com>
2023-01-18 12:11:09 -0500

    create Header component and add styling; change title in index.html

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header className="main-header">
+            <div className="logo">
+                <img className="troll-logo" src="./images/troll-face.png" alt="troll face"/>
+                <h3 className="header-title">Meme Generator</h3>
+            </div>
+            <h4 className="course-title">React Course - Project 3</h4>
+        </header>
+    )
+}
