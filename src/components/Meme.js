846c525f693775292ae9da9cd2cf45df51731835
jholloway <jholloway@griddynamics.com>
2023-01-18 16:12:53 -0500

    change form element to div element

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,13 @@
 export default function Form(){
     return (
         <main>
-            <form className="form-section">
+            <div className="form-section">
                 <div className="input-fields">
                     <input type="text" placeholder="Top text"></input>
                     <input type="text" placeholder="Bottom text"></input>
                 </div>
                 <button className="submit-btn">Get a new meme image  🖼</button>
-             </form>
+             </div>
         </main>
     )
 }
